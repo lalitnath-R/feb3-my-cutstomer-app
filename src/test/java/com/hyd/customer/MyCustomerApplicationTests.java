62450f8af0e5ef5a652096d1b6c3e7c1c0f64868
lalithnath-R <22bk1a05f1@stpetershyd.com>
2025-02-03 11:32:47 +0530

    first commit

---
+++ src/test/java/com/hyd/customer/MyCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyd.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
